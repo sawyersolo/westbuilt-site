d97ed539d590dbda1a4b11c15982761b7d6fd60d
sawyersolo <sawyersolo@gmail.com>
2026-01-02 16:37:08 -0800

    Switch to static Next.js export for Netlify

---
+++ out/_next/static/chunks/app/start/page-1a91c5e05d4b678b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[670],{7154:function(e,n,s){Promise.resolve().then(s.bind(s,9295)),Promise.resolve().then(s.bind(s,6938))}},function(e){e.O(0,[780,223,971,117,744],function(){return e(e.s=7154)}),_N_E=e.O()}]);
